eb0f99c9ea0b54f511df501c206a95a7b7780db6
unidentified <daydaydavid2@gmail.com>
2023-05-20 23:21:12 +0200

    commande help mod
    j'ai changer le fonctionnement de la commande help pour que l'on puisse etre plus specifique.

--- commands.py
+++ commands.py
@@ -14,15 +14,17 @@ def cd(args):
         os.chdir(path)
 
 
-def clear():
+def clear(args):
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def ls(directory='.'):
+def ls(args):
+    import os  # Importer os à l'intérieur de la fonction
+    directory = '.'
     files = os.listdir(directory)
     print(Fore.BLUE + '    '.join(files) + Style.RESET_ALL)
 
-'''
+"""
 def mkdir(args):
     if not args:
         print("mkdir: missing operand")
@@ -31,25 +33,31 @@ def mkdir(args):
         os.makedirs(args[0])
     except FileExistsError:
         print(f"mkdir: cannot create directory '{args[0]}': File exists")
-'''
+"""
+    
+    
+def os(args):
+    if len(args) == 0:
+        print(Fore.RED + "Error: OS command requires arguments" + Style.RESET_ALL)
+    elif args[0] == "-doc":
+        print("OS documentation...")
+    elif args[0] == "-v":
+        print("v0.1.7")
+    elif args[0] == "-info":
+        print("OS info...")
+    else:
+        print(Fore.RED + "Unknown OS command" + Style.RESET_ALL)
 
-def os_doc():
-    url = "https://www.google.com/"
-    print(f"the official documentation for LittleOS : " + Fore.CYAN + url + Style.RESET_ALL)
 
-
-def os_info():
-    print(Fore.BLUE + "your version of LittleOS is a classic version, "
-                      "which means that no software are preinstalled on it " + Style.RESET_ALL)
-
-
-def os_version():
-    print(Fore.BLUE + "LittleOS V0.1.6 " + Style.RESET_ALL)
-
-
-def python_doc():
+def python(args):
     url = "https://docs.python.org/3/library/"
-    print(f"the official documentation for Python : " + Fore.CYAN + url + Style.RESET_ALL)
+    
+    if len(args) == 0:
+        print(Fore.RED + "Error: OS command requires arguments" + Style.RESET_ALL)
+    elif args[0] == "-doc":
+        print(f"the official documentation for Python : " + Fore.CYAN + url + Style.RESET_ALL)
+    else:
+        print(Fore.RED + "Unknown OS command" + Style.RESET_ALL)
 
 
 def rm(file_path):
@@ -68,23 +76,25 @@ def touch(file_path):
         print(f"Erreur: Impossible de créer {file_path}")
 
 
-def unknown_command():
-    print(Fore.RED + "Unknown command" + Style.RESET_ALL)
-
-
-def command_prompt(tool):
-    return f"{tool} $ "
-
-
-def help():
-    print("help : all commands")
-    print("exit : exit program")
-    print("clear : clear screen")
-    print("python -doc : shows the official documentation of Python")
-    print("os -doc : shows the official documentation of littleOS")
-    print("os -v : shows the version of littleOS")
-    print("ls : list all files in current directory")
-    print("cd <directory> : change directory")
-    print("rm <directory> : remove directory")
-    print("mkdir <directory> : create a directory")
-    print("touch <file> : create a file")
+def help(args):
+    if len(args) == 0:
+        print("all the basics commands available. for more specific commands type 'help -[command]' ")
+        print("------------------------------------------------")
+        print("help : all commands")
+        print("exit : exit program")
+        print("clear : clear screen")
+        print("python -[argument] : possible commands to see the version, have information or documentation of Python")
+        print("os -[argument] : possible commands to see the version, have information or documentation of the OS")
+        print("ls : list all files in current directory")
+        print("cd <directory> : change directory")
+        print("rm <directory> : remove directory")
+        print("mkdir <directory> : create a directory")
+        print("touch <file> : create a file")
+    elif args[0] == "-os":
+        print(". -v : show the version of the OS")
+        print(". -doc : show the oficial documentation of the OS")
+        print(". -info : shows useful information about your OS")
+    elif args[0] == "-python":
+        print(". -doc : show the official documentation of python")
+    else:
+        print(Fore.RED + "the argument you want to use is invalid or misspelled" + Style.RESET_ALL)
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ print(Style.RESET_ALL)
 print("this is a beta !")
 time.sleep(0.5)
 
-selected_tool = "classic"
+username = "classic"
 default_path = os.path.dirname(os.path.abspath(__file__))
 
 cmds = {
@@ -27,38 +27,34 @@ cmds = {
     'help': commands.help,
     'ls': commands.ls,
 #   'mkdir': commands.mkdir,
-    'os -doc': commands.os_doc,
-    'os -info': commands.os_info,
-    'os -v': commands.os_version,
-    'python -doc': commands.python_doc,
+    'os': commands.os, # -doc; -info; -v
+    'python': commands.python,
     'rm': commands.rm,
     'touch': commands.touch,
 
 }
 
 
-def unknown_command():
-    print("unknown command")
+def unknown_command(args):
+    print(Fore.RED + "Unknown command" + Style.RESET_ALL)
 
-
-def command_prompt(selected_tool):
+def command_prompt(username):
     directory = os.getcwd().replace('\\', '/')
-    prompt = Fore.GREEN + "⁅" + directory + "⁆" + Fore.CYAN + "\n-{" + selected_tool + "}-> "
+    prompt = Fore.GREEN + "⁅" + directory + "⁆" + Fore.CYAN + "\n-{" + username + "}-> "
     return prompt
-
 
 while True:
     print(Style.RESET_ALL)
-    command = input(Fore.GREEN + command_prompt(selected_tool) + Style.RESET_ALL)
+    command = input(Fore.GREEN + command_prompt(username) + Style.RESET_ALL)
     print(Style.RESET_ALL)
     parts = shlex.split(command)
     cmd = parts[0]
     args = parts[1:]
     if cmd == "cd":
         if args:
-            commands.cd(args)
+            os.chdir(args[0])
         else:
             print("Error: cd command requires an argument")
     else:
         fn = cmds.get(cmd, unknown_command)
-        fn()
+        fn(args)
