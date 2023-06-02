1e5335c1241d31db057c45e72dc8e514f21d772a
qantyk <daydaydavid2@gmail.com>
2023-06-02 21:52:15 +0200

    an other test

--- program/api/command_tool.py
+++ program/api/command_tool.py
@@ -1,9 +1,8 @@
-from variables import commands, errors
+import exeptions
 
-from colorama import Fore, Back, Style
-import shlex
-import os
-import time
+import __init__
+
+
 
 def littleos():
     print(Back.CYAN + Fore.BLACK + "           dainci                                                            ")
@@ -21,37 +20,28 @@ littleos()
 
 time.sleep(0.5)
 
-username = "classic"
-current_file = os.path.dirname(os.path.abspath(__file__))
 
-
-def unknown_command(args):
-    print(f"{Fore.RED}Unknown command{Style.RESET_ALL}")
-
-
-
-def command_prompt(username):
+while True:
+    current_file = os.path.dirname(os.path.abspath(__file__))
     parent_directory = os.path.dirname(current_file)
     os.chdir(parent_directory)
     folders = parent_directory.split(os.sep)
     last_three_folders = folders[-3:]
     path = os.sep.join(last_three_folders).replace('\\', '/')
-    prompt = f"{Fore.GREEN}{'~/.../' if len(folders) > 3 else ''}{path}   {Fore.CYAN}\n=[{username}]=> {Fore.RESET}"
-    return prompt
+    prompt = input(f"├──── {'~/.../' if len(folders) > 3 else ''}{path} ────┤\n└────>")
+    
+    if prompt.lower() == "exit":
+        print("ended by user")
+        time.sleep(0.3)
+        break
 
-try:
-    while True:
-        print(Style.RESET_ALL)
-        command = input(f"{Fore.WHITE}{command_prompt(username)}{Style.RESET_ALL}")
-        print(Style.RESET_ALL)
-        parts = shlex.split(command)
-        cmd = parts[0]
-        args = parts[1:]
-        try:
-            pass
-        except Exception as e:
-            errors.handle_error(cmd, args)
-            print("Error:", e)
-except KeyboardInterrupt:
-    print("Program terminated by user")
-    u = input("enter to quit")
+    try:
+        if hasattr(help, prompt):
+            command_function = getattr(girgo, prompt)
+            command_function()
+        else:
+            print("Commande non reconnue")
+
+    except subprocess.CalledProcessError as e:
+        print("Erreur :", str(e))
+
---
+++ program/main.py
@@ -0,0 +1,1 @@
+from api import command_tool
